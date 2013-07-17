3dae9ec207b8740fac5aeef30f60a61a378f5e93
Apprentice <apprentice@dbc01.local>
2013-07-17 10:40:28 -0700

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComingSoonOverflow::Application.config.secret_token = '38c08e7f29e238f2809530d847a42049d92ba78527e6f8b4212471da5614b3f1bc244790867f9978b755baf75d923760804f988b0f42fd7376295c474d296d2f'
