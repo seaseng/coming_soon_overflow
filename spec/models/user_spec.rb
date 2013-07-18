0190e1356f2197f40d1d14f52860f3ca38c91729
Apprentice <apprentice@dbc01.local>
2013-07-17 17:38:26 -0700

    specs work for bcrypt

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,6 @@ describe User do
   it { should have_many(:votes) }
   it { should validate_presence_of(:email) }
   # it { should validate_presence_of(:password) }
-  # it { should validate_presence_of(:password_confirmation) }
+  it { should validate_confirmation_of(:password) }
   it { should validate_presence_of(:password_digest) }
 end
