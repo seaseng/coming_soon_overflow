57713171729ecdeca1228619e03501f06106afcb
Brian Yee <byee322@gmail.com>
2013-07-17 20:16:21 -0700

    fixed broken head loading

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
-class PostsController < ActionController::Base
+class PostsController < ApplicationController
 	def show
 		@post = Post.all
 	end
+
 	def new
 	end
+
 	def create
 	end
+
 	def update
 	end
 end
