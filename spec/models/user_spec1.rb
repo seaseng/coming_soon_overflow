48f5385243449102e836d43991aad2bae43a75a2
Dean Tambling <dean.tambling@gmail.com>
2013-07-18 11:19:20 -0700

    Deleted user_spec1.rb

--- spec/models/user_spec1.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe User do
-  it { should have_many(:posts) }
-  it { should have_many(:votes) }
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password) }
-  it { should validate_presence_of(:password_confirmation) }
-end
